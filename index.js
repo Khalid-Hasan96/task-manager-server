899c6472065cd6e46f0ecc28b57407d0f3255713
Khalid-Hasan96 <khbappy66@gmail.com>
2022-12-29 20:26:22 +0600

    initializing task manager server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c12imjm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
+
+
+app.get('/', async (req, res) => {
+    res.send('Task Manager Server Working')
+})
+app.listen(port, () => { `Server Running on port ${port}` })
